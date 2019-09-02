193a43149938c690af5f0803d66bf1f990d28307
Jonathan <j.milne21@gmail.com>
2019-09-02 18:55:32 +0000

    Add route to submission form and implemented a temporary function to generate random shortURLs

--- express_server.js
+++ express_server.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny App listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
@@ -30,7 +24,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let result = '';
+  let characters = 'abcbefghijklmnopqurstuvwxyz0123456789';
+  for (let i = 1; i <= 6; i++) {
+    result += characters[Math.floor(Math.random() * characters.length)]
+  } 
+  return result;
+}
