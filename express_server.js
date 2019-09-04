022e182b3ac433cf1f1057583f21971c9751e0cc
Jonathan <j.milne21@gmail.com>
2019-09-04 20:38:17 +0000

    Edit routes to be user specific. User can only get URLs that belong to user. User cannot edit or delete URLs that do not belong to user.

--- express_server.js
+++ express_server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { urlDatabase, users, foundUserFromEmail, generateRandomString } = require("./db");
+const { urlDatabase, users, foundUserFromEmail, generateRandomString, urlsForUser } = require("./db");
 const PORT = 8080;
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-
 
 app.listen(PORT, () => {
   console.log(`Tiny App listening on port ${PORT}!`);
@@ -27,7 +26,6 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
   if (!foundUserFromEmail(req.body.email)) {
     res.status(403).send("Error: Email not found.")
   } else {
@@ -61,37 +59,48 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  
   let user = req.cookies.user_id
-  let templateVars = { urls: urlDatabase, user: users[user]}
+  let templateVars = { urls: urlsForUser(user), user: users[user]}
+  console.log(templateVars)
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new", {user: users[req.cookies.user_id]});
+  if (!req.cookies.user_id) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", {user: users[req.cookies.user_id]});
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id]}
+  let user = req.cookies.user_id
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL , user: users[user], urls: urlsForUser(user)}
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = req.body.longURL;
+  urlDatabase[newShortURL] = { longURL: req.body.longURL, userID: req.cookies.user_id };
   res.redirect(`/urls`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL]
-  res.redirect("/urls")
+  if (urlsForUser(req.cookies.user_id).hasOwnProperty(req.params.shortURL)) {
+    delete urlDatabase[req.params.shortURL];
+  }
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.newURL
-  res.redirect("/urls")
+  if (urlsForUser(req.cookies.user_id).hasOwnProperty(req.params.shortURL)) {
+    urlDatabase[req.params.shortURL].longURL = req.body.newURL;
+  }
+  res.redirect("/urls");
 });
