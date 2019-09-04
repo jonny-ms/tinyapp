18c61059cdc905a82f498014645f75e648dc3d77
Jonathan <j.milne21@gmail.com>
2019-09-04 18:06:14 +0000

    Moved url and user DBs, and helper functions to new module.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const { urlDatabase, users, foundUserFromEmail, generateRandomString } = require("./db");
 const PORT = 8080;
 
 const app = express();
@@ -8,26 +9,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-const users = {
-  abcdef: {
-    id: "abcdef",
-    email: "test@test.com",
-    password: "password"
-  }
-};
-
-
-const foundEmail = function(email) {
-  for (let user in users) {
-    if (users[user].email === email) return user;
-  }
-  return false;
-};
 
 app.listen(PORT, () => {
   console.log(`Tiny App listening on port ${PORT}!`);
@@ -37,16 +18,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
 app.get("/login", (req, res) => {
   res.render("urls_login", {user: users[req.cookies.user_id]});
 });
 
 app.post("/login", (req, res) => {
 
-  if (!foundEmail(req.body.email)) {
+  if (!foundUserFromEmail(req.body.email)) {
     res.status(403).send("Error: Email not found.")
   } else {
-    let user = foundEmail(req.body.email)
+    let user = foundUserFromEmail(req.body.email)
     if (users[user].password !== req.body.password) {
       res.status(403).send("Error: Password incorrect. If you forgot, sorry I can't help you..")
     } else {
@@ -68,7 +53,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   if(!req.body.email || !req.body.password) {
     res.status(400).send("Error: Email and password fields are required for registration.");
-  } else if (foundEmail(req.body.email)) res.status(400).send("Error: Email already assigned to user")
+  } else if (foundUserFromEmail(req.body.email)) res.status(400).send("Error: Email already assigned to user")
   let id = generateRandomString();
   users[id] = { id, email: req.body.email, password: req.body.password};
   res.cookie("user_id", id);
@@ -110,15 +95,3 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.newURL
   res.redirect("/urls")
 });
-
-const generateRandomString = function() {
-  let result = '';
-  let characters = 'abcbefghijklmnopqurstuvwxyz0123456789';
-  for (let i = 1; i <= 6; i++) {
-    result += characters[Math.floor(Math.random() * characters.length)];
-  } 
-  return result;
-};
-
-
-  
